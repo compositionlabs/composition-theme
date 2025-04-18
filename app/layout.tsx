7039b2424b9b583217b0a85af5531aff14ac7310
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2025-04-18 13:49:20 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/assets/metadata_image.png",
-        width: 1200,
-        height: 630,
         alt: "CompLabs Inc.",
       }
     ],
