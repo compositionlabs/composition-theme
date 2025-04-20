e1666212291b2bf07b7708efa921ce1c077ade0b
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2025-04-19 22:39:04 -0700

    Update page.tsx

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main>
         <section id="how-do-i-use-it" className="mt-4 mb-12">
           <h1 className="text-[1.5em] font-bold">How do I use it?</h1>
-          <p className="text-sm leading-relaxed my-3">Finetune our pre-trained model on at least 100 simulations. If your simulations are CFD, now you can run CFD simulations on novel geometries in seconds. The same logic applies for any simulation type.</p>
+          <p className="text-sm leading-relaxed my-3">Finetune our pre-trained model on at least 30 simulations. If your simulations are CFD, now you can run CFD simulations on novel geometries in seconds. The same logic applies for any simulation type.</p>
           <p className="text-sm leading-relaxed my-3">Mach-1.5 was finetuned on 2,500 car aerodynamics simulations.</p>
         </section>
 
