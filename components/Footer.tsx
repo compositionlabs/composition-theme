ef8869de9ea92149c19b6408b37dbf033afffbe2
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2025-06-18 12:27:11 -0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,12 +7,12 @@ export default function Footer() {
         <div className="flex flex-row justify-between pt-4">
             <div className="flex flex-col justify-start footer-content gap-1">
                 <p className="text-gray-500 font-mono text-[0.8em]">Noah Evers</p>
-                <a href="mailto:noah@composition-labs.com" className="text-gray-500 font-mono text-[0.8em]">noah@composition-labs.com</a>
+                <a href="mailto:noah@complabs.com" className="text-gray-500 font-mono text-[0.8em]">noah@complabs.com</a>
                 <a href="https://www.linkedin.com/in/noahevers/" target="_blank" className="text-gray-500 font-mono text-[0.8em]">LinkedIn</a>
             </div>
             <div className="flex flex-col justify-end footer-content gap-1 text-right">
                 <p className="text-gray-500 font-mono text-[0.8em]">Chinmay Shrivastava</p>
-                <a href="mailto:chinmay@composition-labs.com" className="text-gray-500 font-mono text-[0.8em]">chinmay@composition-labs.com</a>
+                <a href="mailto:chinmay@complabs.com" className="text-gray-500 font-mono text-[0.8em]">chinmay@complabs.com</a>
                 <a href="https://www.linkedin.com/in/chinmay-shrivastava-42815b162/" target="_blank" className="text-gray-500 font-mono text-[0.8em]">LinkedIn</a>
             </div>
         </div>
